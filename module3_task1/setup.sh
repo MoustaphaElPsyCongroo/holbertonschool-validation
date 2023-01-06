90e70335efc13935c078672b853946b404197b57
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-01-06 17:41:01 +0100

    1 - Fix apt permissions

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Fixes the production environment
-apt-get update && apt-get install -y hugo wget
+sudo apt-get update && sudo apt-get install -y hugo wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
-apt install ./hugo_0.84.0_Linux-64bit.deb
+sudo apt install ./hugo_0.84.0_Linux-64bit.deb
