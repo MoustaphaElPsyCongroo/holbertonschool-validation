7f284a4a6b063184f546424ada56b49c7eba8694
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-01-06 17:49:48 +0100

    1 - Fix dependencies install

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Fixes the production environment
-sudo apt-get update && sudo apt-get install -y hugo wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
 sudo apt install ./hugo_0.84.0_Linux-64bit.deb
