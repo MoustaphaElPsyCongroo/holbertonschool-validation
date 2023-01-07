0dfdff8b0cb417969aa3fda1d719fecfb9627d96
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-01-07 22:19:32 +0100

    1 - Fix setup.sh

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Fixes the production environment
-wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
-apt-get install ./hugo_0.84.0_Linux-64bit.deb
+apt-get update && apt-get install -y hugo make
+apt-get install curl -y
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+apt-get install ./install_hugo.deb
