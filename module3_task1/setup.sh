dd748f70eeffd76247ca847c701b1338784bee84
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-01-07 22:13:05 +0100

    1 - Fix apt syntax

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Fixes the production environment
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
-sudo apt install ./hugo_0.84.0_Linux-64bit.deb
+apt-get install ./hugo_0.84.0_Linux-64bit.deb
