9c370f372feb7fd01f275349f4c215197618373f
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-01-11 23:58:42 +0100

    2 - Fix syntax

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-# Download lastest release of 'Hugo'
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-
-sudo dpkg -i hugo_extended_0.109.0_linux-amd64.deb
-# Install markdownlint and zip tools
-apt-get install zip -y
-npm install -g markdownlint-cli -y
+apt-get update && apt-get install -y hugo make
+apt-get install curl -y
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+apt-get install ./install_hugo.deb
+npm install -g markdownlint-cli
+rm ./install_hugo.deb
