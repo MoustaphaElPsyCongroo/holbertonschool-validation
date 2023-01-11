fe2258864d6083db8b21c354924994d8fa021138
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-01-11 23:44:30 +0100

    2 - Fix

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make
-apt-get install curl -y
-curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-apt-get install ./install_hugo.deb
+
+# Download lastest release of 'Hugo'
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+
+sudo dpkg -i hugo_extended_0.109.0_linux-amd64.deb
+# Install markdownlint and zip tools
+apt-get install zip -y
+npm install -g markdownlint-cli -y
